ebbcf3af704360d12395442eb7b39031a21d441c
nRamstedt <nikolas.ramstedt@gmail.com>
2022-02-09 11:42:23 +0100

    add screen for testing react-three-arjs

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,2 +1,3 @@
 /* eslint-disable import/prefer-default-export */
 export { default as ThreeDemoScreen } from "@app/screens/ThreeDemoScreen";
+export { default as ReactThreeArJsScreen } from "@app/screens/ReactThreeArJsScreen";
