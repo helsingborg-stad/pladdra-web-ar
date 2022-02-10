66f1c05197bc3ebf4ddf6c84af7d459a26fd1307
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2022-02-10 15:43:41 +0100

    add XR example screen

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,3 +1,4 @@
 /* eslint-disable import/prefer-default-export */
 export { default as ThreeDemoScreen } from "@app/screens/ThreeDemoScreen";
 export { default as ReactThreeArJsScreen } from "@app/screens/ReactThreeArJsScreen";
+export { default as ReactThreeXrScreen } from "@app/screens/ReactThreeXrScreen";
