aeef635b091360c23cc2a9726abb5ca55207dfef
nRamstedt <nikolas.ramstedt@gmail.com>
2022-02-10 19:10:03 +0100

    add routes for GLTF view screen

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,16 @@ import {
   ThreeDemoScreen,
   ReactThreeArJsScreen,
   ReactThreeXrScreen,
+  GLTFViewerScreen,
 } from "@app/screens";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<ThreeDemoScreen />} />
+        <Route path="/" element={<GLTFViewerScreen />} />
+        <Route path="gltf-viewer" element={<GLTFViewerScreen />} />
+        <Route path="three-demo" element={<ThreeDemoScreen />} />
         <Route path="react-three-arjs" element={<ReactThreeArJsScreen />} />
         <Route path="react-three-xr" element={<ReactThreeXrScreen />} />
       </Routes>
